168e69cd8fef5ac89aa6a286fa48a7befd734d4e
lzhou0714 <lzhou0714@g.ucla.edu>
2024-04-19 23:04:18 -0700

    random page

---
+++ laHacks24/src/pages/SignInPage.tsx
@@ -0,0 +1,10 @@
+export const SignInPage = () => {
+    return(
+        <>
+        <h1>SignIn</h1>
+        <button type="button" class="btn btn-primary btn-lg">Signin Here</button>
+
+        </>
+    );
+    
+}
